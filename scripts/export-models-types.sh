024fc8a835c81fe3791d8a268f5979fb9d184851
Erkki Seppälä <flux@inside.org>
2025-09-07 14:22:56 +0300

    export-model-types: use mkdir -p for nicer Dockerfile

--- scripts/export-models-types.sh
+++ scripts/export-models-types.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # So this works out-of-scratch.. Egg vs chicken..
-mkdir frontend/dist
+mkdir -p frontend/dist
 touch frontend/dist/index.html
 TS_RS_EXPORT_DIR=frontend/src/bindings cargo test "$@"
